c76ebd93135251b7c524f4855bb344c5602e0013
Mario-junior95 <karehmario@gmail.com>
2020-09-28 13:11:53 +0300

    Completed step4

---
+++ javascript-basics-02/step 4/step4.js
@@ -0,0 +1,6 @@
+document.querySelector("button").onclick=function(){
+    if(confirm("Do you want to clear all the fields? ") ){
+    document.querySelector("#name").value = "";
+    document.querySelector("#surname").value = "";
+    document.querySelector("#city").value="";
+}};
