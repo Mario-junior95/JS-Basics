b6ac9709fb5463dc3641f756607158332e3ff192
Mario-junior95 <karehmario@gmail.com>
2020-09-29 23:35:26 +0300

    Completed step3

---
+++ javascript-basics-03/step 3/step3.js
@@ -0,0 +1,11 @@
+let txt = document.getElementById("text");
+document.querySelector(".green").onclick = () =>{
+    txt.style.color = "green";
+};
+document.querySelector(".red").onclick = () =>{
+    txt.style.color = "red";
+};
+document.querySelector(".blue").onclick =  () =>{
+    txt.style.color = "blue";
+};
+
