ea2d1379bb35ce63b73904ef20bdf881c821bc8d
Mario-junior95 <karehmario@gmail.com>
2020-09-29 20:58:26 +0300

    Updated step 2

--- javascript-basics-03/step 2/step2.js
+++ javascript-basics-03/step 2/step2.js
@@ -1,14 +1,14 @@
-let txt = document.getElementById("texte")
+let txt = document.getElementById("texte");
 
 document.querySelector("body").addEventListener("click", function(arg1){
-    let target = arg1.target
+    let target = arg1.target;
 
     if (arg1.target.id == "hide"){
-        txt.style.display = "none"
+        txt.style.display = "none";
     }
 
    else if (arg1.target.id == "show"){
-        txt.style.display = "inline"
+        txt.style.display = "inline";
     }
 
 });
