06fecab10fdd85365af3c308eb3d2a1ce66100d6
Mario-junior95 <karehmario@gmail.com>
2020-10-01 19:04:18 +0300

    Updated step 5

--- javascript-basics-02/step 5/step5.js
+++ javascript-basics-02/step 5/step5.js
@@ -1,7 +1,15 @@
-document.querySelector(".container > img").addEventListener("mouseover", function() {
-    document.querySelector("#image1").src = "images/image1_2.jpg";
-    document.querySelector("#image2").src = "images/image2_2.jpg";
-    document.querySelector("#image3").src = "images/image3_2.jpg";
-    document.querySelector("#image4").src = "images/image4_2.jpg";
-    document.querySelector("#image5").src = "images/image5_2.jpg";
+var container = document.querySelector(".container");
+container.addEventListener("mouseover",function(e){
+    var targ = e.target;
+    if(targ.id == "image1"){
+        document.getElementById("image1").src = "images/image1_2.jpg";
+    }else if(targ.id == "image2"){
+        document.getElementById("image2").src = "images/image2_2.jpg";
+    }else if(targ.id == "image3"){
+        document.getElementById("image3").src = "images/image3_2.jpg";
+    }else if(targ.id == "image4"){
+        document.getElementById("image4").src = "images/image4_2.jpg";
+    }else if(targ.id == "image5"){
+        document.getElementById("image5").src = "images/image5_2.jpg";
+    }
 });
